68e888cea282454855c88c151bc8eff501e007bd
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-04-19 08:39:55 -0300

    validate setup

--- progamer/src/main/java/br/com/fiap/bean/SetupBean.java
+++ progamer/src/main/java/br/com/fiap/bean/SetupBean.java
@@ -3,6 +3,8 @@ package br.com.fiap.bean;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import br.com.fiap.dao.SetupDao;
@@ -14,9 +16,15 @@ public class SetupBean {
 
 	private Setup setup = new Setup();
 	
-	public void save() {
+	public String save() {
 		System.out.println(this.setup);
 		new SetupDao().create(setup);
+		
+		FacesContext
+			.getCurrentInstance()
+			.addMessage(null, new FacesMessage("Setup cadastrado com sucesso"));
+		
+		return "setups";
 	}
 	
 	public List<Setup> getAll(){
